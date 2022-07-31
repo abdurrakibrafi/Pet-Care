f0298096ed3155b6edf8af7de05edc41bae3c7f7
abdurrakibrafi <aboudurrakib469@gmail.comm>
2022-07-31 22:54:34 +0600

    Code Added

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDazTtCy5NjAdmtAeu56JVLRwlvrOHnnak",
+  authDomain: "pet-care-a627c.firebaseapp.com",
+  projectId: "pet-care-a627c",
+  storageBucket: "pet-care-a627c.appspot.com",
+  messagingSenderId: "367595755338",
+  appId: "1:367595755338:web:5d4a74bb1712c9ad9d13b3",
 };
 
 // Initialize Firebase
